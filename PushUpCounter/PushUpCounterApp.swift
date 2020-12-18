f9ae29046bd85184d9d8ad6b46a27e73379908ce
ghigbie <gehigbie@gmail.com>
2020-12-17 21:38:04 -0500

    Intial commit

---
+++ PushUpCounter/PushUpCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushUpCounterApp.swift
+//  PushUpCounter
+//
+//  Created by George Higbie on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct PushUpCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
